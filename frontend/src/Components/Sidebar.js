0602fa8c568b271c1aa2d24a20330183ffc99ee4
Ali Saleem Hasan <ali1saleem1hasan@gmail.com>
2021-05-27 09:10:32 +0300

    add react lazy and suspense to app.js

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -14,6 +14,7 @@ import requests from "../handleRequests.js";
 import Close from "@material-ui/icons/Close";
 import { useSocket } from "../SocketProvider.js";
 import { useHistory } from "react-router-dom";
+
 function Sidebar() {
   const [socket] = useSocket();
   const user = useSelector(Selector);
